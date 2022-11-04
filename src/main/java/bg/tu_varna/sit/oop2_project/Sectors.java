433b6a5cea87a553d1dcd5f1c7d91a4bb03b43cc
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 22:47:45 +0200

    Added Sectors class

---
+++ src/main/java/bg/tu_varna/sit/oop2_project/Sectors.java
@@ -0,0 +1,27 @@
+package bg.tu_varna.sit.oop2_project;
+
+public class Sectors {
+    String sectorType;
+    double sectorPrice;
+
+    public Sectors(String sectorType, double sectorPrice) {
+        this.sectorType = sectorType;
+        this.sectorPrice = sectorPrice;
+    }
+
+    public String getSectorType() {
+        return sectorType;
+    }
+
+    public void setSectorType(String sectorType) {
+        this.sectorType = sectorType;
+    }
+
+    public double getSectorPrice() {
+        return sectorPrice;
+    }
+
+    public void setSectorPrice(double sectorPrice) {
+        this.sectorPrice = sectorPrice;
+    }
+}
