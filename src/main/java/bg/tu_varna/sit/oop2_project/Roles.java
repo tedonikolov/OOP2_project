3429b261a60bf09dd3f295906498dd7294da69be
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 22:54:27 +0200

    Added "Roles" class

---
+++ src/main/java/bg/tu_varna/sit/oop2_project/Roles.java
@@ -0,0 +1,17 @@
+package bg.tu_varna.sit.oop2_project;
+
+public class Roles {
+    String roles;
+
+    public Roles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+}
