c12bfa93f87677afcca59d6769eedcf869cb0028
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 23:13:40 +0200

    Seats class added

---
+++ src/main/java/bg/tu_varna/sit/oop2_project/Seats.java
@@ -0,0 +1,47 @@
+package bg.tu_varna.sit.oop2_project;
+
+public class Seats {
+    int amount;
+    int ticketPerClient;
+    Sectors sector;
+    Event event;
+
+    public Seats(int amount, int ticketPerClient, Sectors sector, Event event) {
+        this.amount = amount;
+        this.ticketPerClient = ticketPerClient;
+        this.sector = sector;
+        this.event = event;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getTicketPerClient() {
+        return ticketPerClient;
+    }
+
+    public void setTicketPerClient(int ticketPerClient) {
+        this.ticketPerClient = ticketPerClient;
+    }
+
+    public Sectors getSector() {
+        return sector;
+    }
+
+    public void setSector(Sectors sector) {
+        this.sector = sector;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+}
