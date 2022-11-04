7bcd45a40ed81de23f8d88b09e6435e795462042
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 23:01:38 +0200

    Organiser class added

---
+++ src/main/java/bg/tu_varna/sit/oop2_project/Organiser.java
@@ -0,0 +1,47 @@
+package bg.tu_varna.sit.oop2_project;
+
+public class Organiser {
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+
+    public Organiser(String firstName, String lastName, String email, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
