17c61875b086f9d235e5cde7b16a4383e7d03016
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 23:07:57 +0200

    Event class created

---
+++ src/main/java/bg/tu_varna/sit/oop2_project/Event.java
@@ -0,0 +1,57 @@
+package bg.tu_varna.sit.oop2_project;
+
+public class Event {
+    String name;
+    String address;
+    String date;
+    String description;
+    Organiser profileOrganiser;
+
+    public Event(String name, String address, String date, String description, Organiser profileOrganiser) {
+        this.name = name;
+        this.address = address;
+        this.date = date;
+        this.description = description;
+        this.profileOrganiser = profileOrganiser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Organiser getProfileOrganiser() {
+        return profileOrganiser;
+    }
+
+    public void setProfileOrganiser(Organiser profileOrganiser) {
+        this.profileOrganiser = profileOrganiser;
+    }
+}
