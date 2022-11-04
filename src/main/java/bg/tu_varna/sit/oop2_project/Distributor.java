8bce86e9e8550b49b8d37c9a4d5a9730333cbce0
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 23:18:53 +0200

    edited distributor class

--- src/main/java/bg/tu_varna/sit/oop2_project/Distributor.java
+++ src/main/java/bg/tu_varna/sit/oop2_project/Distributor.java
@@ -1,2 +1,67 @@
-package bg.tu_varna.sit.oop2_project;public class Distributor {
+package bg.tu_varna.sit.oop2_project;
+
+public class Distributor {
+    String firstName;
+    String lastName;
+    String email;
+    String phoneNumber;
+    double rate;
+    double salary;
+
+    public Distributor(String firstName, String lastName, String email, String phoneNumber, double rate, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.rate = rate;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
