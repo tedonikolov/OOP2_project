356434a12376c7538ab0a4d4c7a9de4f7ed9723a
APPetrov <angelpetrovvf@gmail.com>
2022-11-04 23:02:33 +0200

    Edit profiles class

--- src/main/java/bg/tu_varna/sit/oop2_project/Profiles.java
+++ src/main/java/bg/tu_varna/sit/oop2_project/Profiles.java
@@ -1,2 +1,37 @@
-package bg.tu_varna.sit.oop2_project;public class Profiles {
+package bg.tu_varna.sit.oop2_project;
+
+public class Profiles {
+    String username;
+    String password;
+    Roles role;
+
+    public Profiles(String username, String password, Roles role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Roles getRole() {
+        return role;
+    }
+
+    public void setRole(Roles role) {
+        this.role = role;
+    }
 }
