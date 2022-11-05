d21bb59ed8c4f35bbb3ab692ea36c9dd5e7c2cd2
tedonikolov <teonikolov01@abv.bg>
2022-11-05 19:02:52 +0200

    Refactored GetRoles to SelectAll for select query matching all the tables

---
+++ src/main/java/entity/SelectAll.java
@@ -0,0 +1,46 @@
+package entity;
+
+import bg.tu_varna.sit.oop2_project.Database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SelectAll {
+
+    public static List<?> getAll(String table) throws SQLException {
+
+        List<?> list = new ArrayList<>();
+        Connection connection = Database.connection();
+        switch (table){
+            case "ROLES":
+            {
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM ROLES");
+                ResultSet result = statement.executeQuery();
+                List<Role> roles = new ArrayList<>();
+                while (result.next()) {
+                    Role role = new Role(Integer.parseInt(result.getString(1)), result.getString(2));
+                    roles.add(role);
+                }
+                list=roles;
+                break;
+            }
+            case "PROFILES":
+            {
+                PreparedStatement statement = connection.prepareStatement("SELECT * FROM PROFILES JOIN ROLES ON ROLES.ID_ROLE=PROFILES.ROLE_ID");
+                ResultSet result = statement.executeQuery();
+                List<Profile> profiles = new ArrayList<>();
+                while (result.next()) {
+                    Profile profile= new Profile(Integer.parseInt(result.getString(1)), result.getString(2), result.getString(3),new Role(Integer.parseInt(result.getString(5)), result.getString(6)));
+                    profiles.add(profile);
+                }
+                list=profiles;
+                break;
+            }
+        }
+        return list;
+    }
+}
